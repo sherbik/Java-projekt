e10a7b7f7c182d31a8deadb037599d5eb3bdac4c
shrbk <sherbik@gmail.com>
2024-06-29 21:03:28 +0500

    Sale is showing now. Fixed again Product editing(now should compile on every system).

--- src/main/java/net/programa/sklep/programsklep/ShopViewCell.java
+++ src/main/java/net/programa/sklep/programsklep/ShopViewCell.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.layout.HBox;
 import net.programa.sklep.programsklep.Database.Product;
 import net.programa.sklep.programsklep.Database.Setting;
 import net.programa.sklep.programsklep.Database.User;
@@ -19,6 +20,8 @@ public class ShopViewCell {
     public Spinner count;
     public ShopViewController.shopping_cart cart;
     public Label total_price;
+    public Label sale;
+    public HBox sale_box;
 
     public void setValues(Product product, ShopViewController.shopping_cart cart) throws SQLException {
         name.setText(product.name);
@@ -27,6 +30,8 @@ public class ShopViewCell {
         Double pprice = product.price;
         if (User.currentUser.is_regular){
             pprice *= (100 - Double.parseDouble(Setting.get_value("regular_customer_discount", "0"))) / 100;
+            sale.setText(String.format("%.2f", Double.parseDouble(Setting.get_value("regular_customer_discount", "0"))) + "%");
+            sale_box.setVisible(true);
         }
         SpinnerValueFactory.DoubleSpinnerValueFactory cvf = (SpinnerValueFactory.DoubleSpinnerValueFactory) count.getValueFactory();
         cvf.setMax(product.remains);
@@ -39,6 +44,22 @@ public class ShopViewCell {
             try {
                 if (Integer.parseInt(Setting.get_value("whole_sale_from", "9999")) <= ((Double)newValue)){
                     total_price_v *= ((double) (100 - Integer.parseInt(Setting.get_value("whole_sale_discount", "0")))) / 100;
+                    Double d = (100 - Double.parseDouble(Setting.get_value("whole_sale_discount", "0"))) / 100;
+                    Double d2 = ((((100 - Double.parseDouble(Setting.get_value("regular_customer_discount", "0"))) / 100)));
+                    Double total_discount = 1.0;
+                    if (User.currentUser.is_regular){
+                        total_discount*=d;
+                    }
+                    total_discount *=d2;
+                    sale.setText(String.format("%.2f",  (1 -total_discount)*100) + "%");
+                    sale_box.setVisible(true);
+                } else {
+                    if (User.currentUser.is_regular){
+                        sale.setText(String.format("%.2f", Double.parseDouble(Setting.get_value("regular_customer_discount", "0"))) + "%");
+                        sale_box.setVisible(true);
+                    } else {
+                        sale_box.setVisible(false);
+                    }
                 }
             } catch (SQLException e) {
                 utils.showDialog(Alert.AlertType.ERROR, "Error", "Error while updating count", e.getMessage(), rs->null);
